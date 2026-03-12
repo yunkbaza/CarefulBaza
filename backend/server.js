1c8ec216abf146c4013b9ad0bc1dcdce293c979e
yunkbaza <allanbamirati@live.com>
2026-03-12 17:41:56 -0300

    Server atualizado

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { 
+// 🚀 AJUSTE DE DEVOPS: O '0.0.0.0' diz ao Docker para aceitar conexões da AWS!
+app.listen(PORT, '0.0.0.0', () => { 
   console.log(`✅ Servidor Global a rodar na porta ${PORT} com Arquitetura Modular!`); 
 });
